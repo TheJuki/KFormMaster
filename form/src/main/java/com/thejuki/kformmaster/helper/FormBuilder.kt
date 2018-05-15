2996f69b46474a70fe55e9ce03e140ea4ce28acb
Justin Kirk <TheJuki@users.noreply.github.com>
2018-05-15 10:00:01 -0500

    Update FormBuilder to enable cacheForm by default

--- form/src/main/java/com/thejuki/kformmaster/helper/FormBuilder.kt
+++ form/src/main/java/com/thejuki/kformmaster/helper/FormBuilder.kt
@@ -24,7 +24,7 @@ import kotlin.collections.ArrayList
 fun form(context: Context,
          recyclerView: RecyclerView,
          listener: OnFormElementValueChangedListener? = null,
-         cacheForm: Boolean = false,
+         cacheForm: Boolean = true,
          init: FormBuildHelper.() -> Unit): FormBuildHelper {
     val form = FormBuildHelper(
             context = context,
