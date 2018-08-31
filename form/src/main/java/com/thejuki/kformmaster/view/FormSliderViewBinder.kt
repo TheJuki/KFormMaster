e269c87c3e7027e9ac7db21e46f3f80a41f4ef23
Justin Kirk <jkirk@traversconsultingus.com>
2018-08-31 12:22:20 -0500

    Update Seekbar value when the progress changed during the slide. #44

--- form/src/main/java/com/thejuki/kformmaster/view/FormSliderViewBinder.kt
+++ form/src/main/java/com/thejuki/kformmaster/view/FormSliderViewBinder.kt
@@ -47,44 +47,13 @@ class FormSliderViewBinder(private val context: Context, private val formBuilder
         progressValue.text = model.value.toString()
 
         slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateSeekValue(model, slider, progress, progressValue)
+            }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                val sliderValue = seekBar?.progress ?: 0
-                var roundedValue: Int = 0
-                val maximumValue: Double = model.max.toDouble()
-                val minimumValue: Double = model.min.toDouble()
-
-                if (model.steps != null) {
-                    model.steps?.let {
-                        val steps: Double = it.toDouble()
-                        val stepValue: Int = ((sliderValue - minimumValue) / (maximumValue - minimumValue) * steps).roundToInt()
-                        val stepAmount: Int = ((maximumValue - minimumValue) / steps).roundToInt()
-                        roundedValue = stepValue * stepAmount + model.min
-                    }
-                } else if (model.steps == null && model.incrementBy != null) {
-                    model.incrementBy?.let {
-                        val offset = minimumValue % it
-                        val stepValue: Int = ((sliderValue - offset) / it).roundToInt()
-                        roundedValue = stepValue * it + offset.roundToInt()
-                    }
-                } else if (model.steps == null && model.incrementBy == null) {
-                    roundedValue = sliderValue
-                }
-
-                if (roundedValue < model.min) {
-                    roundedValue = model.min
-                } else if (roundedValue > model.max) {
-                    roundedValue = model.max
-                }
-
-                model.error = null
-                model.setValue(roundedValue)
-                formBuilder.onValueChanged(model)
-
-                slider.progress = model.value as Int
-                progressValue.text = model.value.toString()
+                //updateSeekValue(model, slider, seekBar?.progress?: 0, progressValue)
             }
         })
     }, object : ViewStateProvider<FormSliderElement, ViewHolder> {
@@ -96,4 +65,43 @@ class FormSliderViewBinder(private val context: Context, private val formBuilder
             return FormSliderViewState(holder)
         }
     })
+
+    private fun updateSeekValue(model: FormSliderElement,
+                                slider: AppCompatSeekBar,
+                                sliderValue: Int,
+                                progressValue: AppCompatTextView) {
+        var roundedValue = 0
+        val maximumValue: Double = model.max.toDouble()
+        val minimumValue: Double = model.min.toDouble()
+
+        if (model.steps != null) {
+            model.steps?.let {
+                val steps: Double = it.toDouble()
+                val stepValue: Int = ((sliderValue - minimumValue) / (maximumValue - minimumValue) * steps).roundToInt()
+                val stepAmount: Int = ((maximumValue - minimumValue) / steps).roundToInt()
+                roundedValue = stepValue * stepAmount + model.min
+            }
+        } else if (model.steps == null && model.incrementBy != null) {
+            model.incrementBy?.let {
+                val offset = minimumValue % it
+                val stepValue: Int = ((sliderValue - offset) / it).roundToInt()
+                roundedValue = stepValue * it + offset.roundToInt()
+            }
+        } else if (model.steps == null && model.incrementBy == null) {
+            roundedValue = sliderValue
+        }
+
+        if (roundedValue < model.min) {
+            roundedValue = model.min
+        } else if (roundedValue > model.max) {
+            roundedValue = model.max
+        }
+
+        model.error = null
+        model.setValue(roundedValue)
+        formBuilder.onValueChanged(model)
+
+        slider.progress = model.value as Int
+        progressValue.text = model.value.toString()
+    }
 }
