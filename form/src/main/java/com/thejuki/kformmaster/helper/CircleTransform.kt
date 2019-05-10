7e04e9b1e90d7f688946d4fd30c354914dc421d7
douglas-srs <douglas.srs12@gmail.com>
2019-05-10 17:11:28 -0300

    Added border option to CircleTransform

--- form/src/main/java/com/thejuki/kformmaster/helper/CircleTransform.kt
+++ form/src/main/java/com/thejuki/kformmaster/helper/CircleTransform.kt
@@ -4,9 +4,15 @@ import android.graphics.*
 import com.squareup.picasso.Transformation
 
 class CircleTransform : Transformation {
-
     private var x: Int = 0
     private var y: Int = 0
+    private var borderColor : Int? = null
+    private var borderRadius : Int? = null
+
+    constructor(borderColor: Int = Color.WHITE, borderRadius: Int = 2){
+        this.borderColor = borderColor
+        this.borderRadius = borderRadius
+    }
 
     override fun transform(source: Bitmap): Bitmap {
         val size = Math.min(source.width, source.height)
@@ -15,8 +21,11 @@ class CircleTransform : Transformation {
         y = (source.height - size) / 2
 
         val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
-        if (squaredBitmap !== source) source.recycle()
-        val bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+        if (squaredBitmap != source) {
+            source.recycle()
+        }
+
+        val bitmap = Bitmap.createBitmap(size, size, source.config)
 
         val canvas = Canvas(bitmap)
         val paint = Paint()
@@ -24,8 +33,20 @@ class CircleTransform : Transformation {
         paint.shader = shader
         paint.isAntiAlias = true
 
-        val r = size / 2f
-        canvas.drawCircle(r, r, r, paint)
+        if (borderColor != null && borderRadius != null) {
+            val r = size / 2f
+
+            // Prepare the background
+            val paintBg = Paint()
+            paintBg.color = borderColor!!
+            paintBg.isAntiAlias = true
+
+            // Draw the background circle
+            canvas.drawCircle(r, r, r, paintBg)
+
+            // Draw the image smaller than the background so a little border will be seen
+            canvas.drawCircle(r, r, r - borderRadius!!, paint)
+        }
 
         squaredBitmap.recycle()
         return bitmap
